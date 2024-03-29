4ec59eb61b4521438da245fc5020c8905fc48dff
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-03-29 15:04:33 +0600

    dashboard

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const server = http.createServer(app);
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const uri = `mongodb://localhost:27017/softypy`;
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fomplst.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb://localhost:27017/softypy`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fomplst.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
